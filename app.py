e4103aa1d2c2c1d468d7ed451730ccab04201020
Ganesh <59509677+Ganeshpy3@users.noreply.github.com>
2021-05-22 23:38:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import requests
 from flask import Flask,render_template,request,redirect,url_for
 app=Flask(__name__)
 
-# app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 URL="https://cdn-api.co-vin.in/api/v2/admin/location/states"
 
 HEADERS = ({'User-Agent':
